acb0b1961e01093eeef7ff9ebe48c8a342d4275c
felicoper <39249046+felicoper@users.noreply.github.com>
2018-07-12 20:36:19 -0300

    Add files via upload

--- tejotools.py
+++ tejotools.py
@@ -1,48 +1,16 @@
 import csv
-from collections import deque
+import math
+import heapq
 from TDAgrafo import *
 
 """
-La biblioteca a implementar debe tener la siguientes funciones implementadas:
-
-camino_minimo(grafo, desde, hasta): (dijkstra)
-    que nos devuelva una lista con el camino mínimo entre ese par de sedes.
-    Ejemplo: camino_minimo(rusia, 'Moscu', 'Saransk') -> ['Moscu', 'Samara', 'Saransk']
-
-viajante(grafo, origen): *
-    que nos devuelva una lista con el recorrido a hacer para resolver de forma óptima el problema del viajante.
-    La lista debe tener el mismo formato que camino_minimo.
-
-viajante_aproximado(grafo, origen): **
- idem anterior, pero de forma aproximada, siendo este mucho más rápido.
-
-*** = estos dos son uno greedy y otro con backtracking
-
-orden_topologico(grafo): (ot mdf)
-    que nos devuelva una lista con un orden topológico del grafo.
-    En caso de no existir, devolver None, NULL, o el equivalente en el lenguaje elegido. Notar que para esta función, el grafo recibido debe ser dirigido.
-
-arbol_tendido_minimo(grafo): (prim o kruskal)
-    recibe un grafo (que se puede asumir conexo) y devuelve un nuevo grafo
-    que representa un árbol de tendido mínimo del original.
-
+arbol_tendido_minimo(grafo),
+recibe un grafo (que se puede asumir conexo) y devuelve un nuevo grafo,
+que representa un árbol de tendido mínimo del original.
 """
 
-
-"""carga el dataset del .csv al grafo"""
 def parsear_archivo_grafo(file,grafo):
-    """
-    # Cantidad de ciudades (n)
-    Ciudad1,lat1,long1
-    Ciudad1,lat1,long1
-
-    Ciudad_n,lat_n,long_n
-    # Cantidad de aristas
-    Ciudad_i,Ciudad_j,tiempo
-    """
     vertices = {}
-
-    #UNICAMENTE PARSEA LOS VERTICES Y LAS ARISTAS.
     with open (file,"r") as f:
         f_reader = csv.reader(f,delimiter=',')
         for row in f_reader:
@@ -58,59 +26,58 @@ def parsear_archivo_grafo(file,grafo):
                     peso = int(row[2])
                     grafo.agregar_arista(vertice,arista,peso)
 
+    return grafo
+
+def camino_minimo(grafo,inicio,fin):
+    
+    #for v in grafo dist[v] = inf
+    distancia = dict.fromkeys(grafo.obtener_vertices(),math.inf)
+    padre = {}
+    heap = []
+
+    distancia[inicio] = 0
+    padre[inicio] = None
+
+    #ENCOLO UNA TUPLA DE LA FORMA ('MOSCU',DISTANCIA['MOSCU'])
+    heapq.heappush(heap,(inicio,distancia[inicio]))
+
+    while True:
+        try:
+            v = heapq.heappop(heap)
+            for w in grafo.obtener_adyacentes(v[0]):
+                if (distancia[v[0]] + grafo.obtener_peso_arista(v[0],w) < distancia[w]):
+                    padre[w] = v[0]
+                    distancia[w] = distancia[v[0]] + grafo.obtener_peso_arista(v[0],w)
+                    heapq.heappush(heap,(w,distancia[w]))
+        except IndexError:
+            break
+
+    return reconstruir_ciclo(padre, inicio, fin)
+
+
+def reconstruir_ciclo(padre, origen, destino):
+    camino = []
+    actual = destino
+    while actual != origen:
+        camino.insert(0,actual)
+        actual = padre[actual]
+    camino.insert(0,origen)
+    return camino
+
+def arbol_tendido_minimo(grafo):
+    nuevo_grafo = Grafo()
+
+    inicio = grafo.obtener_vertice_random()
+    print(grafo.obtener_nombre_vertice(inicio))
+    
+
+
+    return
 
 def main():
     graph = Grafo()
     parsear_archivo_grafo("sedes.csv",graph)
-
-    print(graph.cantidad_vertices())
-
-
-
-
-
-
-    # graph.agregar_vertice('A','socchi')
-    # graph.agregar_vertice('B','moscu')
-    # graph.agregar_vertice('C','mondongo')
-    # graph.agregar_vertice('D','samara')
-    # graph.agregar_vertice('E','kazan')
-    #
-    # print(graph.obtener_vertice_random())
-    #
-    # print(graph.obtener_dato_vertice('A'))
-    #
-    # #AGREGO ARISTA
-    # if (graph.agregar_arista('A','B',8)):
-    #     print ('AGREGAR ARISTA OK ')
-    #
-    # if (not graph.agregar_arista('A','B',8)):
-    #     print ('FALSE DUPLICAR ARISTA OK')
-    #
-    # graph.agregar_arista('A','C',18)
-    # graph.agregar_arista('A','E',28)
-    # graph.agregar_arista('B','C',38)
-    # graph.agregar_arista('B','D',4)
-    # graph.agregar_arista('C','D',14)
-    # graph.agregar_arista('D','E',24)
-    #
-    # print("A:", graph.obtener_adyacentes('A'))
-    # print("B:",graph.obtener_adyacentes('B'))
-    # print("C:",graph.obtener_adyacentes('C'))
-    # print("D:",graph.obtener_adyacentes('D'))
-    # print("E:",graph.obtener_adyacentes('E'))
-    #
-    #
-    # graph.borrar_arista('A','B')
-    # graph.borrar_vertice('D')
-    # print("debug")
-    # print("A:", graph.obtener_adyacentes('A'))
-    # print("B:",graph.obtener_adyacentes('B'))
-    # print("C:",graph.obtener_adyacentes('C'))
-    # print("D:",graph.obtener_adyacentes('D'))
-    # print("E:",graph.obtener_adyacentes('E'))
-
-
-
+    camino_minimo(graph,'Moscu','Saransk')
+    arbol_tendido_minimo(graph)
 
 main()
